e56f1877700d53b2d2b5409a364d60ed317f255d
عمر عبد السلام <dod998849@gmail.com>
2025-08-27 05:22:08 +0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace To_Do_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.txtbxAddItem = new System.Windows.Forms.TextBox();
+            this.btnShowSelectedItems = new System.Windows.Forms.Button();
+            this.btnRemoveCheckedTask = new System.Windows.Forms.Button();
+            this.btnCheckAllTask = new System.Windows.Forms.Button();
+            this.btnUncheckAllTask = new System.Windows.Forms.Button();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(75, 120);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(530, 194);
+            this.checkedListBox.TabIndex = 0;
+            // 
+            // txtbxAddItem
+            // 
+            this.txtbxAddItem.Location = new System.Drawing.Point(248, 361);
+            this.txtbxAddItem.Name = "txtbxAddItem";
+            this.txtbxAddItem.Size = new System.Drawing.Size(195, 24);
+            this.txtbxAddItem.TabIndex = 1;
+            this.txtbxAddItem.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtbxAddItem_KeyDown);
+            // 
+            // btnShowSelectedItems
+            // 
+            this.btnShowSelectedItems.Location = new System.Drawing.Point(75, 344);
+            this.btnShowSelectedItems.Name = "btnShowSelectedItems";
+            this.btnShowSelectedItems.Size = new System.Drawing.Size(115, 56);
+            this.btnShowSelectedItems.TabIndex = 2;
+            this.btnShowSelectedItems.Text = " Show Selected Items";
+            this.btnShowSelectedItems.UseVisualStyleBackColor = true;
+            this.btnShowSelectedItems.Click += new System.EventHandler(this.btnShowSelectedItems_Click);
+            // 
+            // btnRemoveCheckedTask
+            // 
+            this.btnRemoveCheckedTask.Location = new System.Drawing.Point(75, 442);
+            this.btnRemoveCheckedTask.Name = "btnRemoveCheckedTask";
+            this.btnRemoveCheckedTask.Size = new System.Drawing.Size(115, 56);
+            this.btnRemoveCheckedTask.TabIndex = 3;
+            this.btnRemoveCheckedTask.Text = "Remove Checked Task";
+            this.btnRemoveCheckedTask.UseVisualStyleBackColor = true;
+            this.btnRemoveCheckedTask.Click += new System.EventHandler(this.btnRemoveCheckedTask_Click);
+            // 
+            // btnCheckAllTask
+            // 
+            this.btnCheckAllTask.Location = new System.Drawing.Point(490, 344);
+            this.btnCheckAllTask.Name = "btnCheckAllTask";
+            this.btnCheckAllTask.Size = new System.Drawing.Size(115, 56);
+            this.btnCheckAllTask.TabIndex = 4;
+            this.btnCheckAllTask.Text = "Check All Task";
+            this.btnCheckAllTask.UseVisualStyleBackColor = true;
+            this.btnCheckAllTask.Click += new System.EventHandler(this.btnCheckAllTask_Click);
+            // 
+            // btnUncheckAllTask
+            // 
+            this.btnUncheckAllTask.Location = new System.Drawing.Point(490, 442);
+            this.btnUncheckAllTask.Name = "btnUncheckAllTask";
+            this.btnUncheckAllTask.Size = new System.Drawing.Size(115, 56);
+            this.btnUncheckAllTask.TabIndex = 5;
+            this.btnUncheckAllTask.Text = "Uncheck All Task";
+            this.btnUncheckAllTask.UseVisualStyleBackColor = true;
+            this.btnUncheckAllTask.Click += new System.EventHandler(this.btnUncheckAllTask_Click);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Location = new System.Drawing.Point(281, 420);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(115, 56);
+            this.btnAddTask.TabIndex = 6;
+            this.btnAddTask.Text = "AddTask";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Vladimir Script", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(250, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 57);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "To Do List";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(675, 556);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.txtbxAddItem);
+            this.Controls.Add(this.btnCheckAllTask);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.btnRemoveCheckedTask);
+            this.Controls.Add(this.btnShowSelectedItems);
+            this.Controls.Add(this.btnUncheckAllTask);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = " To Do List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.TextBox txtbxAddItem;
+        private System.Windows.Forms.Button btnShowSelectedItems;
+        private System.Windows.Forms.Button btnRemoveCheckedTask;
+        private System.Windows.Forms.Button btnCheckAllTask;
+        private System.Windows.Forms.Button btnUncheckAllTask;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class Form1 : Form
+    {
+        //بدايه كود توسيط العناصر في الشاشه
+        private Dictionary<Control, CtrlInfo> DefaultControlSizes = new Dictionary<Control, CtrlInfo>();
+        private Size FormDefaultClientSize;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Resize += Form1_Resize;
+            this.WindowState = FormWindowState.Normal;
+            this.MinimumSize = this.Size;
+            FormDefaultClientSize = this.ClientSize;
+
+            Control ctrl = this.GetNextControl(this, true);
+            while (ctrl != null)
+            {
+                if (ctrl is ListBox listBox)
+                    listBox.IntegralHeight = false;
+
+                DefaultControlSizes.Add(ctrl, new CtrlInfo(ctrl.Bounds, ctrl.Font.Size));
+                ctrl = this.GetNextControl(ctrl, true);
+            }
+        }
+
+        private void ScaleControls()
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                foreach (var kvp in DefaultControlSizes)
+                {
+                    Control ctrl = kvp.Key;
+
+                    double Xscl = (double)this.ClientSize.Width / FormDefaultClientSize.Width;
+                    double Yscl = (double)this.ClientSize.Height / FormDefaultClientSize.Height;
+
+                    // لو مش عايز تغير حجم الخط علّق السطرين دول
+                    float fntscl = (float)(kvp.Value.cFontSize * Yscl);
+                    ctrl.Font = new Font(ctrl.Font.FontFamily, fntscl, ctrl.Font.Style, ctrl.Font.Unit);
+
+                    ctrl.Width = (int)(kvp.Value.cBounds.Width * Xscl);
+                    ctrl.Height = (int)(kvp.Value.cBounds.Height * Yscl);
+                    ctrl.Left = (int)(kvp.Value.cBounds.X * Xscl);
+                    ctrl.Top = (int)(kvp.Value.cBounds.Y * Yscl);
+                }
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ScaleControls();
+        }
+
+        public class CtrlInfo
+        {
+            public Rectangle cBounds;
+            public float cFontSize;
+
+            public CtrlInfo(Rectangle bounds, float fontSize)
+            {
+                cBounds = bounds;
+                cFontSize = fontSize;
+            }
+        }
+        // نهايه كود توسيط العناصر في الشاشه
+        string filePath = Path.Combine(Application.StartupPath, "data.txt");
+
+        void LoadDataFromFile()
+        {
+            
+            if (File.Exists(filePath))
+            {
+              checkedListBox.Items.AddRange(File.ReadAllLines(filePath));
+            }
+        }
+        void SaveDataToFile()
+        {
+            string[] Tasks = new string[checkedListBox.Items.Count];
+            for (int i = 0; i < checkedListBox.Items.Count; i++)
+            {
+                Tasks[i]= checkedListBox.Items[i].ToString();
+            }
+            File.WriteAllLines(filePath, Tasks);
+        }
+        void ShowTasks()
+        {
+            if(checkedListBox.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("No tasks selected.", "Info", MessageBoxButtons.OK);
+                return;
+            }
+            string Selected = "";
+            for (int i = 0; i < checkedListBox.CheckedItems.Count; i++)
+            {
+                Selected += (i + 1) + "-" + checkedListBox.CheckedItems[i] + "\n";
+            }
+            MessageBox.Show(Selected, "Selected Items", MessageBoxButtons.OK);
+        }
+        void DeleteTask()
+        {
+            var confirm = MessageBox.Show("Are you sure you want to delete selected tasks?", "Confirm Delete", MessageBoxButtons.YesNo);
+            if (confirm == DialogResult.Yes)
+            {
+                for (int i = checkedListBox.CheckedIndices.Count - 1; i >= 0; i--)
+                {
+                    checkedListBox.Items.RemoveAt(checkedListBox.CheckedIndices[i]);
+                }
+                SaveDataToFile();
+            }
+            
+        } 
+        void AddTask()
+        {
+            if (txtbxAddItem.Text.ToString() == "")
+            {
+                MessageBox.Show("Please Enter A Task", "Nothing To Add", MessageBoxButtons.OK);
+                return;
+            }
+            for (int i = 0; i < checkedListBox.Items.Count; i++)
+            {
+                if (txtbxAddItem.Text.ToLower() == checkedListBox.Items[i].ToString().ToLower())
+                {
+                    MessageBox.Show("Task Already Exist", "Nothing To Add", MessageBoxButtons.OK);
+                    return;
+                }
+            }
+            checkedListBox.Items.Add(txtbxAddItem.Text.ToString());
+            txtbxAddItem.Clear();
+            SaveDataToFile();
+        }
+
+        private void btnShowSelectedItems_Click(object sender, EventArgs e)
+        {
+            ShowTasks();
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+            AddTask();
+        }
+
+        private void btnRemoveCheckedTask_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox.CheckedItems.Count > 0)
+            {
+                DeleteTask();
+                return;
+            }
+            MessageBox.Show("No tasks selected.", "Info", MessageBoxButtons.OK);
+        }
+
+        private void btnCheckAllTask_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox.Items.Count; i++)
+            {
+                checkedListBox.SetItemChecked(i, true);
+            }
+        }
+
+        private void btnUncheckAllTask_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox.Items.Count; i++)
+            {
+                checkedListBox.SetItemChecked(i, false);
+            }
+        }
+
+        private void txtbxAddItem_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnAddTask.PerformClick();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, ""); 
+            }
+            LoadDataFromFile();
+        }
+    }
+
+}
+
+       
+    
+
